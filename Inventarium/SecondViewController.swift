69d647ee0b899b3994793b735df7e297e7c7f9c9
LIU, CHI-YUN <828377@ehs-s-232-03.local>
2018-01-17 11:45:45 -0600

    Initial Commit

---
+++ Inventarium/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  Inventarium
+//
+//  Created by LIU, CHI-YUN on 1/17/18.
+//  Copyright © 2018 LIU, CHI-YUN. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
