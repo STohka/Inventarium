cfd4d0c7a9f0884647bf42b1dd1d2cb77939b59e
LIU, CHI-YUN <828377@ehs-s-232-03.local>
2018-01-18 11:21:55 -0600

    New Inventarium

---
+++ Inventarium/Item.swift
@@ -0,0 +1,29 @@
+//
+//  Item.swift
+//  Inventarium
+//
+//  Created by LIU, CHI-YUN on 1/17/18.
+//  Copyright © 2018 LIU, CHI-YUN. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class Item {
+    
+    var name : String
+    var currentCount : Int
+    var totalCount : Int
+    
+    init?(name: String, currentCount: Int, totalCount: Int) {
+        
+        // The name must not be empty
+        guard !name.isEmpty else {
+            return nil
+        }
+        
+        self.name = name
+        self.currentCount = currentCount
+        self.totalCount = totalCount
+    }
+}
---
+++ Inventarium/ItemsTableViewCell.swift
@@ -0,0 +1,26 @@
+//
+//  ItemsTableViewCell.swift
+//  Inventarium
+//
+//  Created by LIU, CHI-YUN on 1/17/18.
+//  Copyright © 2018 LIU, CHI-YUN. All rights reserved.
+//
+
+import UIKit
+
+class ItemsTableViewCell: UITableViewCell {
+    @IBOutlet weak var itemNameLabel: UILabel!
+
+    @IBOutlet weak var itemCountLabel: UILabel!
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
