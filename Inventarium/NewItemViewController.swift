3c0bb4d3408ffd960204e412cc026b175aad0baf
LIU, CHI-YUN <828377@ehs-s-232-03.local>
2018-01-18 12:28:25 -0600

    discard

---
+++ Inventarium/NewItemViewController.swift
@@ -0,0 +1,50 @@
+//
+//  NewItemViewController.swift
+//  Inventarium
+//
+//  Created by LIU, CHI-YUN on 1/18/18.
+//  Copyright © 2018 LIU, CHI-YUN. All rights reserved.
+//
+
+import UIKit
+
+class NewItemViewController: UIViewController {
+
+    @IBAction func cancel(_ sender: Any) {
+        // Depending on style of presentation (modal or push presentation), this view controller needs to be dismissed in two different ways.
+        let isPresentingInAddMealMode = presentingViewController is UINavigationController
+        
+        if isPresentingInAddMealMode {
+            dismiss(animated: true, completion: nil)
+        }
+        else if let owningNavigationController = navigationController{
+            owningNavigationController.popViewController(animated: true)
+        }
+        else {
+            fatalError("The MealViewController is not inside a navigation controller.")
+        }
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
