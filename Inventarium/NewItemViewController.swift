9566121b62d30b6031f4ba01ed01b8e2f5b93eb2
LIU, CHI-YUN <828377@ehs-s-232-03.local>
2018-01-24 11:45:59 -0600

    new

--- Inventarium/NewItemViewController.swift
+++ Inventarium/NewItemViewController.swift
@@ -9,21 +9,6 @@
 import UIKit
 
 class NewItemViewController: UIViewController {
-
-    @IBAction func cancel(_ sender: Any) {
-        // Depending on style of presentation (modal or push presentation), this view controller needs to be dismissed in two different ways.
-        let isPresentingInAddMealMode = presentingViewController is UINavigationController
-        
-        if isPresentingInAddMealMode {
-            dismiss(animated: true, completion: nil)
-        }
-        else if let owningNavigationController = navigationController{
-            owningNavigationController.popViewController(animated: true)
-        }
-        else {
-            fatalError("The MealViewController is not inside a navigation controller.")
-        }
-    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
