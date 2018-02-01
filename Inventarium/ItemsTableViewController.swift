ddd62c920f61f013a1e6df4159e54569613d59bd
LIU, CHI-YUN <828377@ehs-s-232-03.local>
2018-02-01 11:37:42 -0600

    editing mode done

--- Inventarium/ItemsTableViewController.swift
+++ Inventarium/ItemsTableViewController.swift
@@ -10,12 +10,12 @@ import UIKit
 import os.log
 
 class ItemsTableViewController: UITableViewController {
+    @IBOutlet weak var editButton: UIBarButtonItem!
     
       var items = [Item]()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        navigationItem.leftBarButtonItem = editButtonItem
         loadSampleItems()
 
     }
@@ -89,23 +89,36 @@ class ItemsTableViewController: UITableViewController {
         }
         
     }
-    // Override to support conditional editing of the table view.
-    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the specified item to be editable.
-        return true
+
+    
+    @IBAction func doEdit(sender: AnyObject) {
+        
+        if (self.tableView.isEditing) {
+            editButton.title = "Edit"
+            self.tableView.setEditing(false, animated: true)
+        } else {
+            editButton.title = "Done"
+            self.tableView.setEditing(true, animated: true)
+        }
     }
     
-    
-    
-    // Override to support editing the table view.
     override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
         if editingStyle == .delete {
             // Delete the row from the data source
             items.remove(at: indexPath.row)
-            tableView.deleteRows(at: [indexPath], with: .fade)
+            tableView.deleteRows(at: [indexPath], with: .right)
         } else if editingStyle == .insert {
             // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
         }
     }
+    
+    override func tableView(_ tableView: UITableView, editingStyleForRowAt indexPath: IndexPath) -> UITableViewCellEditingStyle {
+        if tableView.isEditing {
+            return .delete
+        }
+        
+        return .none
+    }
 
-}
+    
+  }
