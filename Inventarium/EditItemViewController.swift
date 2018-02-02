602049325c09b217bc8d580023f76c0a5fb58d4a
LIU, CHI-YUN <828377@ehs-s-232-03.local>
2018-02-02 12:31:54 -0600

    editing segue

--- Inventarium/EditItemViewController.swift
+++ Inventarium/EditItemViewController.swift
@@ -1,35 +1,77 @@
 //
-//  EditItemViewController.swift
+//  NewItemViewController.swift
 //  Inventarium
 //
-//  Created by LIU, CHI-YUN on 2/1/18.
+//  Created by LIU, CHI-YUN on 1/18/18.
 //  Copyright © 2018 LIU, CHI-YUN. All rights reserved.
 //
 
 import UIKit
+import os.log
 
-class EditItemViewController: UIViewController {
+class EditItemViewController: UIViewController, UITextFieldDelegate {
+    @IBOutlet weak var itemNameInput: UITextField!
+    @IBOutlet weak var totalItemCountInput: UITextField!
+    @IBOutlet weak var currentItemCountInput: UITextField!
+    @IBOutlet weak var totalItemCountStepper: UIStepper!
+    @IBOutlet weak var currentItemCountStepper: UIStepper!
+    @IBOutlet weak var saveButton: UIBarButtonItem!
 
+
+    
+    var item : Item?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        itemNameInput.delegate = self
+        
+        // Enable the Save button only if the text field has a valid Meal name.
+        updateSaveButtonState()
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+        
+        super.prepare(for: segue, sender: sender)
+        
+        guard let button = sender as? UIBarButtonItem, button === saveButton else {
+            os_log("The save button was not pressed, cancelling", log: OSLog.default, type: .debug)
+            return
+        }
+        
+        let name = itemNameInput.text ?? ""
+        let currentCount = Int(currentItemCountInput.text!)
+        let totalCount = Int(totalItemCountInput.text!)
+        
+        item = Item(name : name, currentCount : currentCount!, totalCount : totalCount!)
     }
-    */
-
+    
+    func textFieldDidBeginEditing(_ textField: UITextField) {
+        // Disable the Save button while editing.
+        saveButton.isEnabled = false
+        
+    }
+    private func updateSaveButtonState() {
+        // Disable the Save button if the text field is empty.
+        let text = itemNameInput.text ?? ""
+        let number = totalItemCountInput.text ?? ""
+        let isName = !text.isEmpty
+        let isNumber = !number.isEmpty
+        saveButton.isEnabled = isName && isNumber
+    }
+    func textFieldDidEndEditing(_ textField: UITextField) {
+        updateSaveButtonState()
+        
+    }
+    
+    
+    
+    @IBAction func cancel(_ sender: UIBarButtonItem) {
+        dismiss(animated: true, completion: nil)
+    }
+    
 }
--- Inventarium/ItemsTableViewController.swift
+++ Inventarium/ItemsTableViewController.swift
@@ -89,15 +89,63 @@ class ItemsTableViewController: UITableViewController {
         }
         
     }
-
     
+    
+ override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        
+        super.prepare(for: segue, sender: sender)
+    
+        switch(segue.identifier ?? "") {
+           
+        case "ShowDetail":
+            guard let ItemDetailViewController = segue.destination as? EditItemViewController else {
+                fatalError("Unexpected destination: \(segue.destination)")
+            }
+            
+            guard let selectedItemCell = sender as? ItemsTableViewCell else {
+                fatalError("Unexpected sender: \(sender)")
+            }
+            
+            guard let indexPath = tableView.indexPath(for: selectedItemCell) else {
+                fatalError("The selected cell is not being displayed by the table")
+            }
+            
+            let selectedMeal = items[indexPath.row]
+            ItemDetailViewController.item = selectedMeal
+  
+        default:
+            fatalError("Unexpected Segue Identifier; \(segue.identifier)")
+        }
+            
+    
+    }
+func shouldPerformSegueWithIdentifier(identifier: String!, sender: AnyObject!) -> Bool {
+        if identifier == "segueTest" {
+            
+            if (!tableView.isEditing) {
+                
+                return false
+            }
+                
+            else {
+                return true
+            }
+        }
+        return true
+    }
+   
+  
+
+
+
+
     @IBAction func doEdit(sender: AnyObject) {
         
         if (self.tableView.isEditing) {
-            editButton.title = "Edit"
+            editButton.title = "Done"
             self.tableView.setEditing(false, animated: true)
         } else {
-            editButton.title = "Done"
+            
             self.tableView.setEditing(true, animated: true)
         }
     }
