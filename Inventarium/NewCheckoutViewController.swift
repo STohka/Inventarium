b050df2e36c4e3390f44c1cd2eb4c37dd4334977
KEVIN SUN <828198@minint-ke06nvh.district196.org>
2018-03-06 11:22:25 -0600

    checkout

--- Inventarium/NewCheckoutViewController.swift
+++ Inventarium/NewCheckoutViewController.swift
@@ -7,29 +7,17 @@
 //
 
 import UIKit
+import os.log
 
 class NewCheckoutViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
+    @IBOutlet weak var nameField: UITextField!
+    @IBOutlet weak var periodField: UITextField!
+    @IBOutlet weak var typeField: UIPickerView!
+    @IBOutlet weak var quantityField: UILabel!
+    @IBOutlet weak var dateField: UIDatePicker!
+    
+    
+    var item : checkoutItem?
+    
 }
