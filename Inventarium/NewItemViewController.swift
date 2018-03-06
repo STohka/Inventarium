d6023351a1249cc390e8756038fdcb9ae59c5ba1
LIU, CHI-YUN <828377@ehs-s-232-03.local>
2018-03-06 11:21:16 -0600

    text field if statement update

--- Inventarium/NewItemViewController.swift
+++ Inventarium/NewItemViewController.swift
@@ -60,14 +60,13 @@ class NewItemViewController: UIViewController, UITextFieldDelegate {
         // Disable the Save button if the text field is empty.
         let text = itemNameInput.text ?? ""
         let number = itemNumberInput.text ?? ""
-        let isName = !text.isEmpty
-        let isNumber = !number.isEmpty
-        if isName == isNumber{
+
+        if number.isEmpty || text.isEmpty{
         saveButton.isEnabled = true
         }
         else
         {
-            saveButton.isEnabled = false
+            saveButton.isEnabled = true
         }
     }
     
